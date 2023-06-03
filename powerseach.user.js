e52c9034fc92c8c7f09c39af1922da0bfa880193
陶福 <2221057667@qq.com>
2023-06-03 22:27:30 +0800

    init

---
+++ powerseach.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         PowerMarker 
+// @version      0.5.6
+// @description  PowerMarker 
+// @author       Dov
+// @match        https://**
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/jquery.mark.min.js
+// @grant        none
+// ==/UserScript==
+
+
+
+function each(ele){
+    console.log(ele);
+}
+
+var cnt = 0
+function getClassName(){
+    return "powermark-"+ cnt
+}
+
+(function(){
+    window.onload=function(){
+        $("body").mark("Us",{
+                each,
+                className: getClassName()
+        })
+    }
+    window.unmark=function(){
+        $("body") .unmark({className:"xaz-0"})
+    }
+})();
